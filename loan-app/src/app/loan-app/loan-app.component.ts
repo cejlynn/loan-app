6f8ade8d596bf4cbfe8563ceeb3e2aaecda9fb4f
Cait Johnson <caitjohnson@wrightsgate.net>
2023-07-12 21:00:38 -0800

    Add files via upload

---
+++ loan-app/src/app/loan-app/loan-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loan-app',
+  templateUrl: './loan-app.component.html',
+  styleUrls: ['./loan-app.component.css']
+})
+export class LoanAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
